536e4eae2acdb53c0e758712f168b1761c08b08c
Fabian Köbel <fabian.koebel@letsdev.de>
2020-03-20 08:41:10 +0100

    created gradle project.

---
+++ app/src/main/java/de/letsdev/dicoscanner/MainActivity.java
@@ -0,0 +1,14 @@
+package de.letsdev.dicoscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
